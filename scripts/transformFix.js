360bb393461d252c4921fefa6e985900f1a8d20e
Mike <63655075+MixelTe@users.noreply.github.com>
2023-08-23 16:20:25 +0300

    page_utils done

--- scripts/transformFix.js
+++
@@ -1,7 +0,0 @@
-function  tranformFix()
-{
-	document.body.style.setProperty("--tranformFixX", (document.body.clientWidth % 2 == 0 ? 0 : 0.5) + "px");
-	document.body.style.setProperty("--tranformFixY", (document.body.clientHeight % 2 == 0 ? 0 : 0.5) + "px");
-}
-tranformFix()
-setInterval(tranformFix, 1500);
